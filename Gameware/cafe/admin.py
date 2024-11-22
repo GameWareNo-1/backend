3123bb7376d3cdd89fb43f99172034315cb99b6f
alyariamin <aminlyrr@gmail.com>
2024-11-22 16:21:35 +0330

    cafe app + admin panel created

--- Gameware/cafe/admin.py
+++ Gameware/cafe/admin.py
@@ -1,3 +1,23 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models.aggregates import Count
+from django.db.models.query import QuerySet
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
-# Register your models here.
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    list_per_page = 10
+    search_fields = ['title']
+
+class RecipeItemInline(admin.TabularInline):
+    autocomplete_fields = ['ingredient']
+    min_num = 1
+    max_num = 10
+    model = models.RecipeItem
+    extra = 0
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeItemInline]
--- Gameware/cafe/models.py
+++ Gameware/cafe/models.py
@@ -3,9 +3,8 @@ from django.db import models
 
 
 
-class Product(models.Model):
+class Ingredient(models.Model):
     title = models.CharField(max_length=255)
-    slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
 
     def __str__(self) -> str:
@@ -17,12 +16,11 @@ class Product(models.Model):
 
 
 
-class Order(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=255)
 
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+class RecipeItem(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.PROTECT)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
