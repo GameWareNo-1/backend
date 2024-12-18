2f17927feccf5a9df0a5ef4465237f3d52300c4c
msnp1381 <mohamadnematpoor@gmail.com>
2024-12-18 23:12:23 +0330

    changed dir

--- Gameware/Auth/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'is_active', 'is_admin']
