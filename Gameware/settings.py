435efa2feb41cf2f5d957192cae58cf07f30c7cc
alyariamin <aminlyrr@gmail.com>
2024-12-29 21:29:33 +0330

    added level apis

--- Gameware/settings.py
+++ Gameware/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     "Auth",
-    
+    'rest_framework',
+    'level',
     "djoser",
 ]
 
--- Gameware/urls.py
+++ Gameware/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from level.views import LevelViewSet
+level_list = LevelViewSet.as_view({'get': 'list', 'post': 'create'})
+level_detail = LevelViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),  # Optional: Token-based authentication
-
+    path('levels/', level_list, name='level-list'),
+    path('levels/<int:pk>/', level_detail, name='level-detail'),
 ]
