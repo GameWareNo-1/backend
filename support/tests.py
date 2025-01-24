e14f2feca68d9e86b09edbcbfa8383f1194bcd87
mehdi <soroushmehdizade@gmail.com>
2025-01-24 06:57:49 -0800

    reimported the libraries for support app test file

--- support/tests.py
+++ support/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth import get_user_model
+from .models import Message
+from rest_framework.reverse import reverse
 
 # Create your tests here.
