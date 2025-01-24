ca60ca46986657e0bd25a1dc089b39f2cecc1545
mehdi <soroushmehdizade@gmail.com>
2025-01-24 00:12:08 -0800

    reimported requirements in tests.py

--- Auth/tests.py
+++ Auth/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import Group
+from .models import CustomUser
 
 # Create your tests here.
