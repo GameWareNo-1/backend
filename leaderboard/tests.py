733f82d5070c0616662fee0979a11488a593a114
mehdi <soroushmehdizade@gmail.com>
2025-01-24 00:31:27 -0800

    defined the set up method for the app

--- leaderboard/tests.py
+++ leaderboard/tests.py
@@ -4,3 +4,12 @@ from .models import Player
 
 # Create your tests here.
 class LeaderboardTests(APITestCase):
+
+
+    def setUp(self):
+        # Create some players to test with
+        Player.objects.create(name='Player1', score=100)
+        Player.objects.create(name='Player2', score=150)
+        Player.objects.create(name='Player3', score=200)
+        Player.objects.create(name='Player4', score=50)
+        Player.objects.create(name='Player5', score=300)
