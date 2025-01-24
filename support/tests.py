24735e7b614f164a92544cb131556caecba03995
mehdi <soroushmehdizade@gmail.com>
2025-01-24 06:59:47 -0800

    defined set up method

--- support/tests.py
+++ support/tests.py
@@ -6,3 +6,11 @@ from rest_framework.reverse import reverse
 
 # Create your tests here.
 class SupportAppTests(APITestCase):
+
+    # create test users
+    def setUp(self):
+
+        self.user = get_user_model().objects.create_user(username='user1', password='password123')
+        self.admin = get_user_model().objects.create_superuser(username='admin', password='password123')
+
+
