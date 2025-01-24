cccdc1009556bba2cc5415abd480d96ecfd10cb2
mehdi <soroushmehdizade@gmail.com>
2025-01-24 00:19:05 -0800

    completed the player registration test method

--- Auth/tests.py
+++ Auth/tests.py
@@ -10,6 +10,7 @@ class UserRegistrationTests(APITestCase):
         Group.objects.get_or_create(name='Player')
         Group.objects.get_or_create(name='Manager')
 
+
     def test_player_registration_success(self):
         # Test successful player registration
         data = {
@@ -17,3 +18,11 @@ class UserRegistrationTests(APITestCase):
             'password': 'password123',
             'email': 'player1@example.com',
         }
+
+        response = self.client.post('/player/register/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['message'], f"Player user {data['username']} created successfully.")
+        self.assertTrue(CustomUser.objects.filter(username=data['username']).exists())
+        player = CustomUser.objects.get(username=data['username'])
+        self.assertEqual(player.user_type, 'player')
+        self.assertTrue(player.groups.filter(name='Player').exists())
