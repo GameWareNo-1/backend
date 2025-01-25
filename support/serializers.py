1a7c471bbb6309c396729184da48211661a42392
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2025-01-25 15:32:29 +0330

    fix: fix showing username on message

--- support/serializers.py
+++ support/serializers.py
@@ -3,17 +3,19 @@ from .models import Message
 from django.utils.timezone import now
 
 class MessageSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
     class Meta:
         model = Message
-        fields = ['id', 'user', 'message', 'reply', 'created_at', 'replied_at']
-        read_only_fields = ['id', 'created_at', 'replied_at', "message", "user"]
+        fields = ['id', 'user', "username", 'message', 'reply', 'created_at', 'replied_at']
+        read_only_fields = ['id', 'user', "username",'message', 'created_at', 'replied_at']
     def update(self, instance, validated_data):
         if 'reply' in validated_data:
             instance.reply = validated_data['reply']
             instance.replied_at = now()
         instance.save()
         return instance
-    
+    def get_username(self, obj):
+        return obj.user.username if obj.user else None    
 
 class CreateMessageSerializer(serializers.ModelSerializer):
     class Meta:
