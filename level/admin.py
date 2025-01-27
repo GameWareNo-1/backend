319ee81dee4be343c00019c255c7fcafedff416f
infinity <ef.jamshidi017@gmail.com>
2025-01-27 05:21:06 +0330

    add levels to admin panel

--- level/admin.py
+++ level/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Level
 # Register your models here.
+@admin.register(Level)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name' , 'time']
